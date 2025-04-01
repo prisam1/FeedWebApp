bbf5ecd7399c5f738652a9e5de5d37a5a12e3416
Pritam <pritamsamaddar840@gmail.com>
2025-04-01 11:03:46 +0530

    fix auth

--- src/helper/auth.helper.js
+++ src/helper/auth.helper.js
@@ -2,15 +2,16 @@ exports.setAuthCookies = (res, token, isMobile) => {
   const cookieOptions = {
     httpOnly: true,
     sameSite: "none",
-    maxAge: 2 * 24 * 60 * 60 * 1000,  
+    maxAge: 2 * 24 * 60 * 60 * 1000,
+    secure: true,
   };
 
-  if (process.env.NODE_ENV === "production") {
-    cookieOptions.secure = true; // Only secure cookies in production
-  }
-  else {
-    cookieOptions.secure = false; // Allow insecure cookies in local development
-  }
+  // if (process.env.NODE_ENV === "production" || "local") {
+  //   cookieOptions.secure = true; // Only secure cookies in production
+  // }
+  // else {
+  //   cookieOptions.secure = false; // Allow insecure cookies in local development
+  // }
 
   if (!isMobile) {
     res.cookie("access_token", token, cookieOptions);

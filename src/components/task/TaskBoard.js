5517e69c845334f529502341c5ffc41ed47fe3eb
Pritam <pritamsamaddar840@gmail.com>
2025-08-05 11:52:58 +0530

    update

--- src/components/task/TaskBoard.js
+++ src/components/task/TaskBoard.js
@@ -24,7 +24,6 @@ export const TaskBoard = () => {
     );
   };
 
- 
   const openDeleteModal = (taskId) => {
     setTaskToDelete(taskId);
     setIsModalOpen(true);
@@ -47,9 +46,12 @@ export const TaskBoard = () => {
   if (error) return <p>{error}</p>;
 
   return (
-    <div className="flex flex-col w-full lg:max-w-3xl max-w-md lg:ml-0 ml-5">
-      <TaskForm fetchTasks={fetchTasks} /> 
-       <DndProvider backend={HTML5Backend} className="w-full lg:max-w-3xl max-w-md">
+    <div className="flex flex-col w-full lg:max-w-3xl max-w-md lg:ml-0 ml-0">
+      <TaskForm fetchTasks={fetchTasks} />
+      <DndProvider
+        backend={HTML5Backend}
+        className="w-full lg:max-w-3xl max-w-md"
+      >
         <div className="flex flex-row justify-between lg:px-2 gap-1 mt-10">
           {["Pending", "Completed", "Done"].map((status) => (
             <TaskColumn
@@ -62,7 +64,7 @@ export const TaskBoard = () => {
           ))}
         </div>
       </DndProvider>
-    
+
       {/* Modal for delete task */}
       <ConfirmationModal
         isOpen={isModalOpen}

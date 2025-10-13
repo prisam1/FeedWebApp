ac19e76fe72cf133147bfbd778a9d3629352af3f
Pritam <pritamsamaddar840@gmail.com>
2025-10-13 13:16:24 +0530

    Update

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import { ResetPassword } from "../components/forgotPassword/forgotPasswordForm";
 import { useState } from "react";
+import { ArrowLeft, ChevronLeft, ChevronsLeftIcon } from "lucide-react";
 
 export const ForgotPassword = () => {
 
   const [otpSent, setOtpSent] = useState<boolean>(false);
-  
+
   const navigate = useNavigate()
 
   const handleGoBack = () => {
@@ -16,9 +17,10 @@ export const ForgotPassword = () => {
     <div>
       <button
         onClick={handleGoBack}
-        className="mt-40 ml-10 absolute items-center bg-purple-300 rounded-md border px-2 py-1"
+        className="mt-40 ml-10 absolute items-center flex gap-2 bg-purple-300 rounded-md border px-2 py-1.5"
       >
-        ← back
+        <ArrowLeft height={24} width={24} />
+        Back
       </button>
       <div className="flex flex-col h-[90vh] items-center justify-center w-full lg:p-8 p-4">
 
@@ -27,12 +29,12 @@ export const ForgotPassword = () => {
             Reset your password
           </h2>
           <p className="text-[#111827] mt-[4vw] md:mt-[16px] text-[4.5vw] md:text-lg dark:text-gray-500">
-           {otpSent? "Please check your registered email address to reset your password.": `Please press "Send OTP" to receive a password reset code in your registered email.`}
+            {otpSent ? "Please check your registered email address to reset your password." : `Please press "Send OTP" to receive a password reset code in your registered email.`}
           </p>
         </div>
-        <ResetPassword 
-        otpSent={otpSent}
-        setOtpSent={setOtpSent}      
+        <ResetPassword
+          otpSent={otpSent}
+          setOtpSent={setOtpSent}
         />
       </div>
     </div>
